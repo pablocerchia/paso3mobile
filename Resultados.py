45d80e04ce71aff108560f3822af9efd1bfe3f09
pablocerchia <88912256+pablocerchia@users.noreply.github.com>
2023-10-04 10:42:15 -0300

    Update Resultados.py

--- Resultados.py
+++ Resultados.py
@@ -34,26 +34,7 @@ st.markdown(
        """,
         unsafe_allow_html=True,
     )
-st.write("#")
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 tabs_generales = sac.tabs(['PRESIDENTE','DIPUTADOS','SENADORES', 'GOBERNADOR PBA', 'MESA'], index=0, format_func='upper', height=None, align='center', position='top', shape='default', grow=False, return_index=False)
